c1743b5bec66e645769bfe689e2eff74abab6679
dzemildupljak <dzemildupljak4795@gmail.com>
2020-03-24 14:18:32 +0100

    Add files via upload

---
+++ QuarantineVezba4.py
@@ -0,0 +1,91 @@
+# broj = int(input("Unesite neki broj: "))
+#
+# if broj % 7 == 0:
+#     print("Broj je deljiv sa sedam")
+#
+# else:
+#     print("Broj nije deljiv sa sedam")
+#@#########################
+# broj = float(input("Unesite realan broj: "))
+#
+# if broj == 0:
+#     print('broj je jednak 0')
+# elif broj < 0:
+#     print("broj je negativan")
+# else:
+#     print('broj je pozitivan')
+#####################################################33#
+# a = int(input("a"))
+# b = int(input("b"))
+#
+# if a+b >= 20:
+#     print("zbir je veci ili jednak 20")
+###########################################################
+# a = int(input("a: "))
+# b = int(input("b: "))
+# c = int(input("c: "))
+#
+# if a == b and a == c:
+#     print("Jednkakostranican")
+# elif a == b or b == c or c == a:
+#     print('Jenakokraki')
+# else:
+#     print("Raznostranican")
+
+# print("Unosimo policu broj" , 1)
+# print("Unisumo policu broj" , 2)
+# print("Unisumo policu broj" , 3)
+# print("Unisumo policu broj" , 4)
+# print("Unisumo policu broj" , 5)
+# print("Unisumo policu broj" , 6)
+# print("Unisumo policu broj" , 7)
+# print("Unisumo policu broj" , 8)
+# print("Unosumo policu broj" , 9)
+# print("Unosumo policu broj" , 10)
+
+# for i in range(1,11):
+#     print('Unosumo policu broj', i)
+# ####################################################################33
+#
+# zbir = 0
+#
+# for i in range(2, 21, 2):
+#     zbir = zbir + i
+#     print("Posle broja ", i, "zbir je: ", zbir)
+################################################################
+# broj_b = int(input("Koliko broje hocemo da unesemo: "))
+# parni = 0
+# neparni = 0
+#
+# for i in range(0, broj_b):
+#     broj = int(input("Unesite broj: "))
+#     if broj % 2 == 0:
+#         parni = parni + 1
+#     else:
+#         neparni = neparni + 1
+#
+# print("Broj parnig brojeva", parni)
+# print("Broj neparnih brojeva", neparni)
+######################################################################
+# broj = int(input("UNesite broj"))
+#
+# for i in range(1, broj+1):
+#     if broj % i == 0:
+#         print(i, end=' ')
+#
+##################################################
+# sifra = ''
+#
+# while sifra != '123':
+#     sifra = input("Unesite sifru: ")
+#     if sifra == '123':
+#         print("Unesli ste ispravnu sifru!")
+#     else:
+#         print("Uneli ste pogresnu sifru!")
+
+
+
+
+
+
+
